b0e999da33b36e972cdfd88fee90d323abff6ec6
Lam Voong <lkv@berkeley.edu>
2023-02-01 09:45:59 -0800

    HMP-270: revert UCB pahma_refresh.sh to main dir; add LYR pahma_refresh.sh to pahma dir

--- pahma_refresh.sh
+++ pahma_refresh.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-export BASEDIR=${HOME}/cspace-solr-ucb/pahma
+export BASEDIR=${HOME}/cspace-utils-ucb/pahma
 export PGUSER=nuxeo_pahma
 export PGPASSWORD="${PAHMA_PGPASSWORD}" # apply via SSM param store
 export PGDATABASE=pahma_domain_pahma
 export PGHOST="${PAHMA_PGHOST}"
 export PGPORT="${PAHMA_PGPORT}"
-export EMAIL_FROM=${PAHMA_EMAIL_FROM}
-export CONTACT="${PAHMA_CONTACT}"
 
 time psql -q -t -c "select utils.refreshculturehierarchytable();"
 time psql -q -t -c "select utils.refreshmaterialhierarchytable();"
 time psql -q -t -c "select utils.refreshtaxonhierarchytable();"
 time psql -q -t -c "select utils.refreshobjectplacelocationtable();"
+time psql -q -t -c "select utils.refreshobjectclasshierarchytable();"
 
-cd ${BASEDIR} ; psql -q -t -f checkstatus.sql | mail -r ${EMAIL_FROM} -s "hierarchies refresh" "${CONTACT}"
