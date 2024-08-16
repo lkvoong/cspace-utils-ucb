f04352b09889adb74d70a54e97f968e54b577989
Lam Voong <lkv@berkeley.edu>
2024-08-16 10:52:23 -0700

    CSC-2352: Fix join errors for numberofobjects/objectcount schema change

--- pahma/hierarchies/object_location/CreateObjectPlaceTable.sql
+++ pahma/hierarchies/object_location/CreateObjectPlaceTable.sql
@@ -9,14 +9,14 @@ $$
   FROM (
     SELECT
       cc.id,
-      hc.name collectionobjectcsid,
+      hcc.name collectionobjectcsid,
       ocg.objectcount objectcount,
       cc.objectnumber numberofobjects,
       pn.placecsid placecsid
     FROM collectionobjects_common cc
     JOIN misc m ON (cc.id = m.id AND m.lifecyclestate <> 'deleted')
     JOIN hierarchy hcc ON (cc.id = hcc.id)
-    LEFT OUTER JOIN hierarchy hocg ON (cc.id = hocg.id AND hocg.primarytype = 'objectCountGroup' AND hocg.pos = 0)
+    LEFT OUTER JOIN hierarchy hocg ON (cc.id = hocg.parentid AND hocg.primarytype = 'objectCountGroup' AND hocg.pos = 0)
     LEFT OUTER JOIN objectcountgroup ocg ON (hocg.id = ocg.id)
     LEFT OUTER JOIN collectionobjects_pahma_pahmafieldcollectionplacelist pl ON (cc.id = pl.id AND pl.pos = 0)
     LEFT OUTER JOIN places_common pc ON (pl.item = pc.refname)
--- pahma/hierarchies/object_location/CreateObjectPlaceView.sql
+++ pahma/hierarchies/object_location/CreateObjectPlaceView.sql
@@ -8,15 +8,15 @@ $$
   SELECT
     cc.id,
     hcc.name collectionobjectcsid,
-    cc.objectcount numberofobjects,
+    ocg.objectcount numberofobjects,
     cc.objectnumber objectnumber,
     pn.placecsid placecsid
   FROM collectionobjects_common cc
-  JOIN misc m ON (c.id = m.id AND m.lifecyclestate <> 'deleted')
+  JOIN misc m ON (cc.id = m.id AND m.lifecyclestate <> 'deleted')
   JOIN hierarchy hcc ON (cc.id = hcc.id)
-  LEFT OUTER JOIN hierarchy hocg ON (cc.id = hocg.id AND hocg.primarytype = 'objectCountGroup' AND hocg.pos = 0)
+  LEFT OUTER JOIN hierarchy hocg ON (hcc.id = hocg.parentid AND hocg.primarytype = 'objectCountGroup' AND hocg.pos = 0)
   LEFT OUTER JOIN objectcountgroup ocg ON (hocg.id = ocg.id)
-  JOIN collectionobjects_pahma_pahmafieldcollectionplacelist pl ON (c.id = pl.id AND pl.pos = 0)
+  JOIN collectionobjects_pahma_pahmafieldcollectionplacelist pl ON (cc.id = pl.id AND pl.pos = 0)
   JOIN places_common pc ON (pl.item = pc.refname)
   JOIN hierarchy hpc ON (pc.id = hpc.id AND hpc.primarytype = 'PlaceitemTenant15')
   JOIN utils.placename_hierarchy pn ON (hpc.name = pn.placecsid)
