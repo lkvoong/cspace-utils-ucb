9e36ce629f0cc358b3f715950fa33bcf42b3f80c
Lam Voong <lkv@berkeley.edu>
2025-09-18 13:50:18 -0700

    CSW-979: remove unused joins

--- cinefiles/scripts/sql/denorm_nightly/filmdocs.sql
+++ cinefiles/scripts/sql/denorm_nightly/filmdocs.sql
@@ -6,32 +6,29 @@
 -- after all of the denorm tables have been successfully created.
 --
 -- Modified GLJ 8/3/2014
+-- LKV 09/18/2025 remove unused join to hierarchy
 
 DROP TABLE IF EXISTS cinefiles_denorm.filmdocstmp;
 
 CREATE TABLE cinefiles_denorm.filmdocstmp AS
-   SELECT
-      wc.shortidentifier film_id,
-      cast(co.objectnumber AS bigint) doc_id,
-      'not used'::char(8) AS entered,
-      'not used'::char(8) AS modified,
-      'not used'::char(8) AS entered_by,
-      'not used'::char(8) AS verified_by,
-      'not used'::char(8) AS note
-   FROM
-      hierarchy h1
-      INNER JOIN collectionobjects_common co
-         ON (h1.id = co.id AND h1.primarytype = 'CollectionObjectTenant50')
-      INNER JOIN misc m
-         ON (co.id = m.id AND m.lifecyclestate <> 'deleted')
-      INNER JOIN collectionobjects_cinefiles_filmsubjects ccf
-         ON (co.id = ccf.id)
-      INNER JOIN works_common wc
-         ON (wc.refname=ccf.item)
-   WHERE (co.objectnumber ~ '^[0-9]+$')
-     AND ccf.item IS NOT NULL
-     AND ccf.item <> ''
-   ORDER BY wc.shortidentifier, cast(co.objectnumber AS bigint);
+  SELECT
+    wc.shortidentifier film_id,
+    cast(co.objectnumber AS bigint) doc_id,
+    'not used'::char(8) AS entered,
+    'not used'::char(8) AS modified,
+    'not used'::char(8) AS entered_by,
+    'not used'::char(8) AS verified_by,
+    'not used'::char(8) AS note
+  FROM collectionobjects_common co
+  JOIN misc m ON (
+    co.id = m.id
+    AND m.lifecyclestate <> 'deleted')
+  JOIN collectionobjects_cinefiles_filmsubjects ccf ON co.id = ccf.id
+  JOIN works_common wc ON wc.refname = ccf.item
+  WHERE co.objectnumber ~ '^[0-9]+$'
+  AND ccf.item IS NOT NULL
+  AND ccf.item <> ''
+  ORDER BY wc.shortidentifier, cast(co.objectnumber AS bigint);
 
 GRANT SELECT ON cinefiles_denorm.filmdocstmp TO GROUP reporters_cinefiles;
 GRANT SELECT ON cinefiles_denorm.filmdocstmp TO reader_cinefiles;
